5344f95c153e5927f303ec141a3bf72a62a9f01a
Tiago Pereira <tiagorp2004@gmail.com>
2024-04-12 01:15:27 +0100

    2 scopes

--- appAzure/src/authConfig.js
+++ appAzure/src/authConfig.js
@@ -54,15 +54,7 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-  scopes: [
-    'User.Read',
-    'Mail.Read',
-    'Mail.Send',
-    'Mail.ReadBasic',
-    'Mail.ReadBasic.All',
-    'Mail.ReadWrite',
-    'User.Read.All',
-  ],
+  scopes: ['User.Read', 'Mail.Read'],
 };
 
 /**
